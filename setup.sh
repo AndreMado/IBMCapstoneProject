b9181a1ffc0b1457d850679b5fdd59c66e32845d
AndreMado <andresxmaldonado@gmail.com>
2025-02-04 17:09:56 -0600

    feat: automated MySQL data import with SQL script

--- setup.sh
+++ setup.sh
@@ -2,10 +2,12 @@
 
 set -e  # Terminar si hay un error
 echo "Validating all the required data in the system"
+cd data
 if ! ls | grep -q "oltpdata.csv"; then
 	echo "Downloading data"
 	wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0321EN-SkillsNetwork/oltp/oltpdata.csv
 fi
+cd ..
 # Verificar si Minikube está corriendo
 echo "Welcome to my Capstone project :) by Andres Maldonado"
 echo "Checking Minikube status..."
@@ -29,3 +31,10 @@ echo "Waiting for MySQL pod to be ready..."
 kubectl wait --for=condition=ready pod -l app=mysql --timeout=120s
 
 echo "MySQL deployment completed successfully!"
+
+# Ejecutar script SQL en MySQL
+echo "Running SQL script to create and populate the database..."
+kubectl exec -it $(kubectl get pod -l app=mysql -o jsonpath="{.items[0].metadata.name}") -- \
+    mysql -u root -p$(kubectl get secret mysql-secret -o jsonpath="{.data.MYSQL_ROOT_PASSWORD}" | base64 --decode) < sql/OLTPdb.sql
+
+echo "Database and data import completed successfully!"
