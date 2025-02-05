4898e2160a2e73e117765ae58bb3d96b0a9d26f3
AndreMado <andresxmaldonado@gmail.com>
2025-02-04 18:20:34 -0600

    feat: completed MySQL setup with automated data import and export

--- sql/OLTPdb.sql
+++ sql/OLTPdb.sql
@@ -17,3 +17,19 @@ INTO TABLE sales_data
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 (product_id, customer_id, price, quantity, time_stamp);
+
+SET @index_exists = (
+    SELECT COUNT(*)
+    FROM INFORMATION_SCHEMA.STATISTICS
+    WHERE table_schema = 'sales'
+    AND table_name = 'sales_data'
+    AND index_name = 'idx_time_stamp'
+);
+
+SET @query = IF(@index_exists = 0, 'CREATE INDEX idx_time_stamp ON sales_data(time_stamp);', 'SELECT "Index already exists";');
+
+PREPARE stmt FROM @query;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+SHOW INDEX FROM sales_data;
